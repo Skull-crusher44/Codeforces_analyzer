5a6f218b427f44ee26c089218fac466cb8af69be
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-08-12 21:48:12 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span>
+                Developed by <a href="https://codeforces.com/profile/Skull6393"  target="_blank">Pankaj Ahuja</a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
